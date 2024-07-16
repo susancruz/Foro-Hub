f529fa8873573118e2900988190aae7a3b8cd909
Susan Cruz P <157200679+susancruz@users.noreply.github.com>
2024-07-15 19:01:55 -0600

    Create DatosCrearRespuesta.java

---
+++ src/main/java/com/forohub/api/domain/respuestas/DatosCrearRespuesta.java
@@ -0,0 +1,7 @@
+package com.je.forohub.api.domain.respuestas;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DatosCrearRespuesta(
+    @NotNull String contenido, 
+    @NotNull Long idForo ) {}
