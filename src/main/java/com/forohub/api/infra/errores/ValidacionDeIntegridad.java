2b57cbc76cc989478fef2e5fda8e477aa6ccbf18
Susan Cruz P <157200679+susancruz@users.noreply.github.com>
2024-07-15 19:08:13 -0600

    Create ValidacionDeIntegridad.java

---
+++ src/main/java/com/forohub/api/infra/errores/ValidacionDeIntegridad.java
@@ -0,0 +1,9 @@
+package com.je.forohub.api.infra.errores;
+
+public class ValidacionDeIntegridad extends RuntimeException {
+
+    public ValidacionDeIntegridad(String s) {
+        super(s);
+        
+    }
+}
