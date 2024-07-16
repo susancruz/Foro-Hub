4d4b7376f11f6be938379ec06da60ce08c53b15a
Susan Cruz P <157200679+susancruz@users.noreply.github.com>
2024-07-15 19:04:37 -0600

    Create DatosAutenticacionUsuario.java

---
+++ src/main/java/com/forohub/api/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,9 @@
+package com.je.forohub.api.domain.usuarios;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DatosAutenticacionUsuario(
+    @NotNull String usuario,
+    @NotNull String clave) {
+
+ }
