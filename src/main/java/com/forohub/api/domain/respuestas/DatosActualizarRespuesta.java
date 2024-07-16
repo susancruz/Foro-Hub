7a163f5d51eb37c4e905f3fcb8a5d4bfbde1958c
Susan Cruz P <157200679+susancruz@users.noreply.github.com>
2024-07-15 19:01:20 -0600

    Create DatosActualizarRespuesta.java

---
+++ src/main/java/com/forohub/api/domain/respuestas/DatosActualizarRespuesta.java
@@ -0,0 +1,9 @@
+package com.je.forohub.api.domain.respuestas;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DatosActualizarRespuesta(
+    @NotNull Long id, 
+    @NotNull String contenido) {
+
+}
