24b7acd58afc2bfa58ba73d4435f26c1baf6dab7
Susan Cruz P <157200679+susancruz@users.noreply.github.com>
2024-07-15 19:02:21 -0600

    Create DatosRespuesta.java

---
+++ src/main/java/com/forohub/api/domain/respuestas/DatosRespuesta.java
@@ -0,0 +1,10 @@
+package com.je.forohub.api.domain.respuestas;
+
+public record DatosRespuesta(Long id, String contenido, Long idTopico) {
+
+	public DatosRespuesta(Respuesta respuesta) {
+
+		this(respuesta.getId(), respuesta.getContenido(), respuesta.getForo().getId());
+		
+	}
+}
